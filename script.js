596423c1aff42b0f83467b656523ee23b928a42c
gusmor94 <gusmor94@users.noreply.github.com>
2020-10-17 19:37:26 -0700

    saved time block text to localStorage

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+tasks = {};
+
 // display current day at top of calendar
 var displayDate = function() {
     var currentDate = moment().format("dddd, LL");
@@ -8,10 +10,43 @@ displayDate();
 
 // present time bocks with standard business hours
 
-// color code time blocks for past, present, or future
+// save time blocks into localStorage
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+
+var saveTasks = function() {
+    
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 
 // edit time blocks
+$(".time-text").on("click", "p", function() {
+    var text = $(this).text().trim();
+    var textInput = $("<textarea>").addClass("form-control form-control-sm").val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+})
 
-// save time blocks into localStorage
+$(".time-text").on("blur", "textarea", function() {
+    var time = $(".time-text").data("time");
+    var text = $(this).val().trim();
+    // recreate p element
+    var timeText = $("<p>").addClass("h-100 w-100").attr("id", time).text(text);
+    $(this).replaceWith(timeText);
 
-// load saved time blocks 
+    $(".btn").on("click", function() {
+        var buttonTime = $(this).attr("id");
+        text = text;
+        tasks[buttonTime] = text;
+        saveTasks();
+    })
+})
+
+// color code time blocks for past, present, or future
+
+
+// load saved time blocks
+loadTasks();
